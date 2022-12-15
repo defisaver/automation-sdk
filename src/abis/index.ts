d9de8457ec0dbccbd5f1aac27b17d277ad5c2d4d
cussone <aleksandar.cakalic@gmail.com>
2022-12-15 10:32:26 +0100

    Add centralized export file to `/abis`

---
+++ src/abis/index.ts
@@ -0,0 +1,28 @@
+import type { ContractJson } from '../types';
+
+import MakerSubscriptions from './legacy_MakerSubscriptions.json';
+import AaveV2Subscriptions from './legacy_AaveV2Subscriptions.json';
+import CompoundV2Subscriptions from './legacy_CompoundV2Subscriptions.json';
+
+import Erc20 from './Erc20.json';
+import UniMulticall from './UniMulticall.json';
+import SubStorage from './SubStorage.json';
+import AuthCheck from './legacy_AuthCheck.json';
+
+function castToContractJsonType(json: any) {
+  return json as ContractJson;
+}
+
+const MakerSubscriptionsJson = castToContractJsonType(MakerSubscriptions);
+const AaveV2SubscriptionsJson = castToContractJsonType(AaveV2Subscriptions);
+const CompoundV2SubscriptionsJson = castToContractJsonType(CompoundV2Subscriptions);
+
+const Erc20Json = castToContractJsonType(Erc20);
+const UniMulticallJson = castToContractJsonType(UniMulticall);
+const SubStorageJson = castToContractJsonType(SubStorage);
+const AuthCheckJson = castToContractJsonType(AuthCheck);
+
+export {
+  MakerSubscriptionsJson, AaveV2SubscriptionsJson, CompoundV2SubscriptionsJson,
+  Erc20Json, UniMulticallJson, SubStorageJson, AuthCheckJson,
+};
