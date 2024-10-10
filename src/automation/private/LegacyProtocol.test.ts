2e88ee8cb311c5c2393f7f43fa57c505653c0dd5
Dimitrije Glibic <dimitrije.glibic1@gmail.com>
2024-10-10 13:58:03 +0200

    Test failure

--- src/automation/private/LegacyProtocol.test.ts
+++ src/automation/private/LegacyProtocol.test.ts
@@ -5,11 +5,7 @@ import { ProtocolIdentifiers } from '../../types/enums';
 import LegacyProtocol from './LegacyProtocol';
 
 describe('Feature: LegacyProtocol.ts', () => {
-  describe('When testing class LegacyProtocol', () => {
-    const exampleProtocol = new LegacyProtocol({ id: ProtocolIdentifiers.LegacyAutomation.MakerDAO });
-    const examples: Array<[string, string]> = [
-      [exampleProtocol.id, 'MakerDAO'],
-      [exampleProtocol.name, 'MakerDAO'],
+  
       [exampleProtocol.slug, 'makerdao'],
       [exampleProtocol.version, ''],
       [exampleProtocol.fullName, 'MakerDAO'],
